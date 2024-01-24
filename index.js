65d04d85e9208160496c07b04d8a3f56086f50e7
Maica <maicat1990@gmail.com>
2024-01-24 02:42:13 -0300

    asi?

--- index.js
+++ index.js
@@ -1,35 +1,83 @@
-const carrito = []
+let carrito = []
 
-const eventosDisponibles = document.querySelector("#eventos-disponibles")
-const verCarrito = document.querySelector("#ver-carrito")
-const cantidadAdulto = document.querySelector("#cantidad-adultos")
-const totalAdulto = cantidadAdulto * eventos.precioAdulto
-const cantidasMenor = document.querySelector("#cantidad-menores")
-const totalMenor = cantidasMenor * eventos.precioMenor
-const totalCompra = totalAdulto + totalMenor
+const contenedorEventos = document.querySelector("#eventos-disponibles")
+const botonAgregar = document.querySelector("#button")
+const botonVerCarrito = document.querySelector("#ver-carrito")
+const carritoContenedor = document.querySelector("#carrito-contenedor")
 
-
-verCarrito.addEventListener("click", () => {
-    carrito.forEach((item) => {
-        const div = document.createElement('div')
-        div.classList.add("item")
-        div.innerHTML += `
-            <p>Evento: ${item.nombre}</p>
-            <p>Adultos: ${cantidadAdulto}</p>
-            <p>Menores: ${cantidasMenor}</p>
-            <p>El total de tu compra es: ${totalCompra}    
-    `
+function mostrarEventos() {
+    eventos.forEach(evento => {
+        contenedorEventos.innerHTML += `
+        <option>"${evento.nombre}" - Adultos: $${evento.precioAdulto} - Menores: $${evento.precioMenor}</option>
+    `    })
+    botonAgregar.addEventListener("click", () => {
+        const cantidadAdulto = document.querySelector("#cantidad-adultos").valueAsNumber || 0
+        const cantidadMenor = document.querySelector("#cantidad-menores").valueAsNumber || 0
+        const eventoSeleccionado = eventos[contenedorEventos.selectedIndex]
+        agregarAlCarrito(eventoSeleccionado, cantidadAdulto, cantidadMenor)
     })
-    verCarrito.appendChild(div)
+}
+mostrarEventos()
+function agregarAlCarrito(evento, cantidadAdulto, cantidadMenor) {
+    
+    if (eventoYaEnCarrito(evento.id)) {
+        carrito.forEach(item => {
+            if (item.eventoId === evento.id) {
+                item.cantidadAdulto += cantidadAdulto;
+                item.cantidadMenor += cantidadMenor;
+            }
+        })
+    } else {
+        const item = {
+            eventoId: evento.id,
+            nombre: evento.nombre,
+            precioAdulto: evento.precioAdulto,
+            precioMenor: evento.precioMenor,
+            cantidadAdulto,
+            cantidadMenor
+        }
+
+        carrito.push(item);
+    }
+
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    verCarrito();
+}
+
+botonVerCarrito.addEventListener("click", () => { 
+    verCarrito()
 })
 
+function verCarrito() {
+    carritoContenedor.innerHTML = ''
 
-eventos.forEach((evento) => {
-    eventosDisponibles.innerHTML += `
-        <option>"${evento.nombre}" - Adultos: $${evento.precioAdulto} - Menores: $${evento.precioMenor} </option>
-    `
-    const button = document.querySelector("#button")
-    button.addEventListener("click", () =>
-    carrito.push(evento))
+    carrito.forEach(item => {
+        const itemsDiv = document.createElement('div')
+        itemsDiv.innerHTML = `
+        <p>${item.nombre}</p>
+        <p>Adultos: ${item.cantidadAdulto}</p>
+        <p>Menores: ${item.cantidadMenor}</p>
+        <p>Total: ${calcularTotal(item)}</p>
+        `
+        carritoContenedor.appendChild(itemsDiv)
+    })
+}
 
-})
+function calcularTotal(item) {
+    return (item.cantidadAdulto * (item.precioAdulto || 0)) + (item.cantidadMenor * (item.precioMenor || 0))    
+}
+function eventoYaEnCarrito(eventoId) {
+    return carrito.some(item => item.eventoId === eventoId)
+}
+
+const carritoGuardado = localStorage.getItem('carrito')
+
+if (carritoGuardado) {
+    carrito = JSON.parse(carritoGuardado);
+} else {
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+        
+verCarrito()
+
